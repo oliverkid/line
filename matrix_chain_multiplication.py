678202cbed92362cf942fd4fb4296757b9de64ec
oliverkid <106012087+oliverkid@users.noreply.github.com>
2023-05-22 01:19:12 +0800

    Update matrix_chain_multiplication.py

--- matrix_chain_multiplication.py
+++ matrix_chain_multiplication.py
@@ -4,15 +4,20 @@ import time
 import matplotlib.pyplot as plt
 
 #Brute-Force_recursive 
-def MatrixChainOrder0(p, i, j):
-	if i == j:
-		return 0
-	_min = sys.maxsize
-	for k in range(i, j):
-		count = (MatrixChainOrder0(p, i, k)+ MatrixChainOrder0(p, k+1, j)+ p[i-1] * p[k] * p[j])
-		if count < _min:
-			_min = count
-	return _min
+def MatrixChainOrder0(p,n): 
+  s = [[0] * n for _ in range(n)]  
+  def matrix_chain_recursive(i, j):
+    if i == j:
+      return 0
+    _min = sys.maxsize
+    for k in range(i, j):
+      cost = matrix_chain_recursive(i, k) + matrix_chain_recursive(k + 1, j) + (p[i-1] * p[k] * p[j])
+      if cost < _min:
+        _min = cost
+        s[i][j] = k 
+    return _min
+  min_scalar_multiplications = matrix_chain_recursive(1, n-1)
+  return min_scalar_multiplications,s
 
 #Dynamic Programming
 def matrixChainOrder1(p,n):
@@ -33,12 +38,12 @@ def matrixChainOrder1(p,n):
 
 t_recursive = []
 t_dynamic = []
-for t in range(2,16):
+for t in range(2,17):
 	P = [random.randint(1,30) for _ in range(t)]
 	n = len(P)
  
 	t_start = time.time()
-	m1 = MatrixChainOrder0(P, 1, n-1)
+	m1,s1 = MatrixChainOrder0(P, n)
 	t_end = time.time()
 	t_recursive.append(t_end-t_start)
  
@@ -47,7 +52,7 @@ for t in range(2,16):
 	t_end = time.time()
 	t_dynamic.append(t_end-t_start)
 
-x = [i for i in range(2,16)]
+x = [i for i in range(2,17)]
 plt.xlabel("Input_size")
 plt.ylabel("Execution time")
 plt.plot(x, t_recursive, 'r')
